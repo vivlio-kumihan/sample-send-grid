18d41f9b617763384c9b10dba7863cd019ae4f22
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-11-30 10:41:29 +0900

    sendGrid cant set up

--- src/App.js
+++ src/App.js
@@ -1,101 +1,73 @@
 import React from 'react';
-import { Row, Col, Form, Input, Button, Space, notification } from 'antd';
-import 'antd/dist/antd.css';
-
-const { TextArea } = Input;
-
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('YOUR_SENDGRID_API_KEY');
+import { Row, Col, Form, Input, Space, Button } from 'antd';
+// import 'antd/dist/antd.css';
+import './App.css';
 
 function App() {
-	const [form] = Form.useForm();
+  const { TextArea } = Input;
+  const [form] = Form.useForm();
 
-	const onComplete = (fields) => {
-		const message = {
-			to: 'wigayam256@forfity.com',
-			from: fields.email,
-			subject: fields.subject,
-			html: `
-      <p><strong>Name:</strong> ${fields.name}</p>
-      <p>${fields.message}</p>`,
-		};
+  // const sgMail = require('@sendgrid/mail');
+  // sgMail.setApiKey("SG.7D79sCDaQZWwUZwHmiz4Vw.sOH1bVNpZsUUZO_-rNdpNAWe-ra7b2m7dxO9Vuod1SA");
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey('SG.7D79sCDaQZWwUZwHmiz4Vw.sOH1bVNpZsUUZO_-rNdpNAWe-ra7b2m7dxO9Vuod1SA');
 
-		sgMail
-			.send(message)
-			.then(() => {
-				form.resetFields();
-				console.log('Email Sent!');
-				notification.open({
-					message: 'Message successfu!',
-					description: 'We have successfully received your email.',
-				});
-			})
-			.catch((error) => {
-				console.error('Error: ', error);
-			});
-	};
+  return (
+    <div className="App">
+      <Row gutter={24} style={{padding: "30px"}}>
+        <Col xl={12}>
+          <Form form={form} layout='vertical'>
+            <Form.Item
+              name="name"
+              label="name"
+              rules={[
+                {required: true}
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-	return (
-		<Row gutter={24} style={{ padding: '30px' }}>
-			<Col xl={12}>
-				<Form layout='vertical' form={form} onFinish={onComplete}>
-					<Form.Item
-						name='name'
-						label='Name'
-						rules={[
-							{
-								required: true,
-							},
-						]}>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						name='email'
-						label='Email'
-						rules={[
-							{
-								required: true,
-							},
-						]}>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						name='subject'
-						label='Subject'
-						rules={[
-							{
-								required: true,
-							},
-						]}>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						name='message'
-						label='Message'
-						rules={[
-							{
-								required: true,
-							},
-						]}>
-						<TextArea />
-					</Form.Item>
-					<Form.Item>
-						<Space>
-							<Button type='primary' htmlType='submit'>
-								Submit
-							</Button>
-							<Button
-								type='secondary'
-								htmlType='submit'
-								onClick={(e) => form.resetFields()}>
-								Clear
-							</Button>
-						</Space>
-					</Form.Item>
-				</Form>
-			</Col>
-		</Row>
-	);
+            <Form.Item
+              name="email"
+              label="email"
+              rules={[
+                {required: true}
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            
+            <Form.Item
+              name="subject"
+              label="subject"
+              rules={[
+                {required: true}
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            
+            <Form.Item
+              name="message"
+              label="message"
+              rules={[
+                {required: true}
+              ]}
+            >
+              <TextArea />
+            </Form.Item>
+
+            <Form.Item>
+              <Space>
+                <Button type="primary">Submit</Button>
+                <Button onClick={(e) => form.resetFields()}>Clear</Button>
+              </Space>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default App;
